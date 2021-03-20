c1205a6b2e32b294e798ca335d36c8034cc4c351
Carlo <Carlo@DESKTOP-BUVSFQG.local>
2021-03-20 19:53:10 +0100

    Actualizar SigueAlJugador.cs y SigueAlJugador.cs.meta

---
+++ Assets/Scripts/SigueAlJugador.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SigueAlJugador : MonoBehaviour
+{
+    
+public GameObject jugador;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.position = jugador.transform.position + new Vector3 (0,4,-6);  
+    }
+}
